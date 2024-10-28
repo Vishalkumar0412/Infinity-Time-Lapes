fa3872297efa268055f89afb68c5e8ac5c1065bc
vishal kumar <vishakumar041200@gmail.com>
2024-10-28 18:22:25 +0530

    timerChanged

--- src/Components/Mainpage/Mainpage.jsx
+++ src/Components/Mainpage/Mainpage.jsx
@@ -8,10 +8,10 @@ function Mainpage() {
     useEffect(()=>{
         const timer = setInterval(() => {
             
-          const date= new Date().toLocaleDateString()
+         
           const time= new Date().toLocaleTimeString('en-US',{hour12:true})
-          const dateTime = `${date} ${time}`
-          setCurrentTime(dateTime)
+       
+          setCurrentTime(time)
           }, 1000);
       
           // Cleanup the interval on component unmount
@@ -22,7 +22,7 @@ function Mainpage() {
       <section className='bg-gradient-to-b from-gray-100 to-gray-200 flex justify-center items-center h-screen flex-col'> 
     
     <h1 className="md:text-9xl text-7xl font-primary text-red-600 m-2 p-2 font-bold  text-center "> Infinity <span><CgInfinity className='inline' /> </span>  Time Lapse</h1>
-   <h3 className="md:text-5xl text-2xl text-red-700 m-2 p-2 font-bold text-center"> A Complete Time Management </h3>
+   <h3 className="md:text-5xl text-2xl text-red-700 m-2 p-2 font-bold text-center"> A Complete Time Management Solution</h3>
    <h2 className="md:text-3xl text-2xl text-red-700 m-2 p-2 font-bold text-center"> “I am Time, the great destroyer of the world."</h2>
      <h3 id="TimeH3" className="md:text-5xl text-center cursor-pointer font-bold shadow-xl shadow-red-700  text-4xl text-white bg-red-800 m-2 p-4">{currentTime} </h3>
     
